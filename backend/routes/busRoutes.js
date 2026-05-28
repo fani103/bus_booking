0bfed5a138f4ce5a888c84bb2da89c7f24d62694
Faneesh Boya <263159965+fani103@users.noreply.github.com>
2026-05-28 11:03:53 +0530

    Delete backend directory

--- backend/routes/busRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { getBuses, seedBuses } = require("../controllers/busController");
-
-router.get("/", getBuses);
-router.get("/seed", seedBuses);
-
-module.exports = router;
--- backend/routes/userRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const { loginOrCreateUser } = require("../controllers/userController");
-
-const router = express.Router();
-
-router.post("/login", loginOrCreateUser);
-
-module.exports = router;
